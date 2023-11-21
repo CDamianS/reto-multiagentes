74d4de2e71e5147971c34698b0374c851b8f2fa1
CDamianS <A01656277@tec.mx>
2023-11-21 17:07:21 -0600

    Inicio de mapa.py

---
+++ mapa.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import mesa
+
+edificios = []
+
+calle = []
+
+puntos_llegada_peatones = []
+
+puntos_salida_peatones = []
+
+puntos_llegada_autos = []
+
+puntos_salida_autos = []
